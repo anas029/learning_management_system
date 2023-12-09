6c18f2f2288f42f4c75c77a8858a03665022d3e7
anas029 <aliyaqoob+github@gmail.com>
2023-12-10 00:59:17 +0300

    clerk auth

---
+++ app/providers/Provider.tsx
@@ -0,0 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+	return <ClerkProvider>{children}</ClerkProvider>;
+}
